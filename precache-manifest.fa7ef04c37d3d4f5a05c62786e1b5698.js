b5e580d9077339c2aca80ca65988c2b4ece9bede
codissa <hishmehissa@gmail.com>
2021-12-06 00:46:07 +0200

    Updates

---
+++ precache-manifest.fa7ef04c37d3d4f5a05c62786e1b5698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b93abba9da6427edead4162ec209cc",
+    "url": "/Pplfinder/index.html"
+  },
+  {
+    "revision": "d4f86344894365998e0c",
+    "url": "/Pplfinder/static/js/2.da51d363.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/Pplfinder/static/js/2.da51d363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb18c4eb34a38c9cffa",
+    "url": "/Pplfinder/static/js/main.d2bbb10b.chunk.js"
+  },
+  {
+    "revision": "a4f6b1ba3a53173eee53",
+    "url": "/Pplfinder/static/js/runtime-main.c57070c1.js"
+  }
+]);
