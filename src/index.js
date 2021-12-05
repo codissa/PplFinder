acedaab4059392fd165f0bd193dafc22233039f1
codissa <hishmehissa@gmail.com>
2021-12-06 00:23:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "AppRouter";
 
-ReactDOM.render(<AppRouter />, document.querySelector("#root"));
+ReactDOM.render(<React.StrictMode><AppRouter /> </React.StrictMode>, document.querySelector("#root"));
